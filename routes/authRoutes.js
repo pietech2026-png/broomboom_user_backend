6bc363949a10ef44a646ec0262829d882560a73b
pietech2026-png <rhythm.shesha@gmail.com>
2026-05-28 16:55:34 +0530

    added auth route test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const { login, getProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
+router.get('/check', (req, res) => {
+   res.json({
+      success: true,
+      message: "Auth Routes Working"
+   });
+});
+
 router.post('/login', login);
 router.get('/profile', protect, getProfile);
 
